35ff2932e9e9b0b934c87702fad07070d1411fc5
Loui Sakaki <e1352207@outlook.jp>
2018-03-14 00:28:02 +0900

    Enable to long tap +- buttons.

--- app/src/main/java/jp/louis/nsr/simplecounter/widgets/HoldableButton.java
+++ app/src/main/java/jp/louis/nsr/simplecounter/widgets/HoldableButton.java
@@ -1,11 +1,23 @@
 package jp.louis.nsr.simplecounter.widgets;
 
 import android.content.Context;
+import android.os.Handler;
 import android.support.v7.widget.AppCompatButton;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class HoldableButton extends AppCompatButton {
+    private int delayMsec = 400;
+    private int intervalMsec = 100;
+
+    private MainTask mainTask = new MainTask() {
+        @Override
+        public void doTask() {}
+    };
+    private Handler handler = new Handler();
+    private Runnable runnableCode;
+
     public HoldableButton(Context context) {
         this(context, null);
     }
@@ -14,9 +26,9 @@ public class HoldableButton extends AppCompatButton {
     }
     public HoldableButton(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        initListener();
     }
-
-    public void setMainTask(final MainTask mainTask) {
+    private void initListener() {
         setOnClickListener(
             new OnClickListener() {
                 @Override
@@ -25,6 +37,42 @@ public class HoldableButton extends AppCompatButton {
                 }
             }
         );
+
+        //
+        runnableCode = new Runnable() {
+            @Override
+            public void run() {
+                mainTask.doTask();
+                handler.postDelayed(runnableCode, intervalMsec);
+            }
+        };
+        setOnTouchListener(
+            new OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    switch(motionEvent.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        handler.postDelayed(runnableCode, delayMsec);
+                        break;
+
+                    case MotionEvent.ACTION_UP:
+                        handler.removeCallbacks(runnableCode);
+                        view.performClick();
+                        break;
+
+                    default:
+                        break;
+                    }
+
+                    return true;
+                }
+            }
+        );
+    }
+
+    public void setMainTask(MainTask mainTask) {
+        this.mainTask = mainTask;
+        initListener();
     }
 
     abstract public static class MainTask {
